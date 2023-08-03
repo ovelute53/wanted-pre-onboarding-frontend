c134e347c155917a0e624e66967aa526517d033c
ovelute53 <ovelute53@naver.com>
2023-08-03 20:48:42 +0900

    :adhesive_bandage: 불필요한 코드 제거 후 컴포넌트 연결

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Signup from './Signup';
+import Signin from './Signin';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
