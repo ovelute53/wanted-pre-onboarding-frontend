a370b1c156b7c58cd7ab6b72e12ef3f313f8178d
ovelute53 <ovelute53@naver.com>
2023-08-05 11:17:59 +0900

    :white_check_mark: component 적용 후 sign up/in 수정

--- src/pages/SigninPage/Signin.jsx
+++ src/pages/SigninPage/Signin.jsx
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../../Signup.css';
+import { SignForm } from '../../components/SignForm/SignForm';
 
 
 function Signin (){
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
+  const navigate = useNavigate();
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (formData) => {
     try {
       // API 주소
       const apiUrl = 'https://www.pre-onboarding-selection-task.shop/';
@@ -28,6 +16,8 @@ function Signin (){
       const response = await axios.post(apiUrl, formData);
       console.log('API 호출 성공');
       console.log('응답 데이터:', response.data);
+
+      navigate('/todo')
     } catch (error) {
       console.error('API 호출 에러', error);
     }
@@ -36,34 +26,8 @@ function Signin (){
   return (
     <div>
       <h1>SIGN IN</h1>
-      <form onSubmit={handleSubmit}>
-        <div className='input-box'>
-          <label>이메일</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            data-testid="email-input"
-            required
-          />
-        </div>
-        <div className='input-box'>
-          <label>비밀번호</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            data-testid="password-input"
-            required
-          />
-        </div>
-        <div className='button-box'>
-          <button type="submit" data-testid="signin-button">로그인</button>
-          <p><Link to='/'>회원이 아니신가요?</Link></p>
-        </div>      
-      </form>
+      <SignForm buttonText='로그인' onSubmit={handleSubmit} />
+      <p><Link to='/'>회원이 아니신가요?</Link></p>
     </div>
   )
 }
--- src/pages/SignupPage/Signup.jsx
+++ src/pages/SignupPage/Signup.jsx
@@ -1,59 +1,12 @@
-import React, { useState } from 'react';
+import React, { Children, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import '../../Signup.css';
+import { SignForm } from '../../components/SignForm/SignForm';
 
 function Signup() {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const [errors, setErrors] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const validateEmail = (email) => {
-    if (!email.includes('@')) {
-      return '이메일 형식이 올바르지 않습니다.';
-    } 
-    return '';
-  };
-
-  const validatePassword = (password) => {
-    if (password.length < 8) {
-      return '비밀번호는 8자리 이상이어야 합니다.';
-    }
-    return '';
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    // 아이디 비밀번호 유효성 검사
-    const emailError = validateEmail(formData.email);
-    const passwordError = validatePassword(formData.password);
-    setErrors({
-      email: emailError,
-      password: passwordError,
-    });
-
-    if (emailError || passwordError) {
-      return;
-    }
-
+  const handleSubmit = async (formData) => {
     try {
       //API 주소
       const apiUrl = 'https://www.pre-onboarding-selection-task.shop/';
@@ -73,46 +26,8 @@ function Signup() {
   return (
     <div>
       <h1>SIGN UP</h1>
-      <form onSubmit={handleSubmit}>
-        <div className='input-box'>
-          <label>이름</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            data-testid="username-input"
-            required
-          />
-        </div>
-        <div className='input-box'>
-          <label>이메일</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            data-testid="email-input"
-            required
-          />
-        </div>
-        <div className='input-box'>
-          <label>비밀번호</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            data-testid="password-input"
-            required
-          />
-          <div className="error">{errors.password}</div>
-        </div>
-        <div className='button-box'>
-          <button type="submit" data-testid="signup-button" disabled={!!errors.email || !!errors.password}>회원가입</button>
-          <p><Link to='/signin'>이미 회원이신가요?</Link></p>
-        </div>
-        </form>
+      <SignForm buttonText='회원가입' onSubmit={handleSubmit} >{Children}</SignForm>
+      <p><Link to='/signin'>이미 회원이신가요?</Link></p>
     </div>
   );
 }
