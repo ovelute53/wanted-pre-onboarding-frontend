045656741a7d3a35c34c7c34e4f9ce539a3ff15c
ovelute53 <ovelute53@naver.com>
2023-08-08 23:24:26 +0900

    :white_check_mark: BrowserRouter -> HashRouter로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './global.css';
 import Signup from './pages/SignupPage/Signup';
 import Signin from './pages/SigninPage/Signin';
+import Todo from './pages/TodoPage/Todo';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Signup />}/>
           <Route path='/signin' element={<Signin />}/>
+          <Route path='/todo' element={<Todo />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
