4708b03e049fa8ee7d610b9c27260c0940865770
ovelute53 <ovelute53@naver.com>
2023-08-08 23:23:38 +0900

    :white_check_mark: Util 함수로 분리

---
+++ src/utils/todoUtils.js
@@ -0,0 +1,38 @@
+// todoUtils.js
+import { api } from '../utils/api';
+
+export const getTodos = async () => {
+  try {
+    const response = await api.get('/todos');
+    return response.data;
+  } catch (error) {
+    console.error('API 호출 에러: ', error);
+  }
+};
+
+export const createTodo = async (newTodo) => {
+  try {
+    const requestBody = { todo: newTodo };
+    const response = await api.post('/todos', requestBody);
+    return response.data;
+  } catch (error) {
+    console.error('API 호출 에러: ', error);
+  }
+};
+
+export const deleteTodo = async (id) => {
+  try {
+    await api.delete(`/todos/${id}`);
+  } catch (error) {
+    console.error('API 호출 에러: ', error);
+  }
+};
+
+export const updateTodo = async (id, editValue) => {
+  try {
+    const requestBody = { todo: editValue, isCompleted: false };
+    await api.put(`/todos/${id}`, requestBody);
+  } catch (error) {
+    console.error('API 호출 에러:', error);
+  }
+};
