a5016339e88feba324e68dfb59243bd1b65dbe48
ovelute53 <ovelute53@naver.com>
2023-08-10 11:38:30 +0900

    :recycle: url 경로에 '#'이 추가되는 것을 HashRouter -> BrowserRouter로 변경하여 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './global.css';
 import Signup from './pages/SignupPage/Signup';
 import Signin from './pages/SigninPage/Signin';
@@ -7,13 +7,13 @@ import Todo from './pages/TodoPage/Todo';
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <Router>
         <Routes>
           <Route path='/' element={<Signup />}/>
           <Route path='/signin' element={<Signin />}/>
           <Route path='/todo' element={<Todo />}/>
         </Routes>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
