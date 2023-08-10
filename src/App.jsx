85980801735e4bb453b38178e8026c621e23d3f0
ovelute53 <ovelute53@naver.com>
2023-08-10 13:24:12 +0900

    :rocket: HashRouter로 변경 후 test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './global.css';
 import Signup from './pages/SignupPage/Signup';
 import Signin from './pages/SigninPage/Signin';
@@ -7,7 +7,7 @@ import Todo from './pages/TodoPage/Todo';
 function App() {
   return (
     <div className="App">
-      <Router basename='/wanted-pre-onboarding-frontend'>
+      <Router>
         <Routes>
           <Route path='/' element={<Signup />}/>
           <Route path='/signin' element={<Signin />}/>
